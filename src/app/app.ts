55943c0d8c1b6d26609ab80e07ea0fb074427077
Stefan Simić <stefansimic@device-198.home>
2026-02-23 10:32:28 +0100

    add base structure and custom fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreeceAthens } from './greece-athens/greece-athens';
+import { HungaryBudapest } from './hungary-budapest/hungary-budapest';
+import { ItalyRome } from './italy-rome/italy-rome';
+import { SerbiaSumadija } from './serbia-sumadija/serbia-sumadija';
+import { CroatiaPula } from './croatia-pula/croatia-pula';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    GreeceAthens,
+    HungaryBudapest,
+    ItalyRome,
+    SerbiaSumadija,
+    CroatiaPula
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
